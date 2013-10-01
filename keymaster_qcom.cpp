2e270e95835f89dec04078e1ea2d4ff16adbc84d
Andrew Warfield <hiasant@gmail.com>
2013-09-30 19:22:18 -0700

    Revert "Merge "Use canonical UniquePtr.h header""
    This reverts commit e35dafa88f1ba15a30b95556755ab17c7207be9d, reversing
    changes made to 70789f277025960f499d4155ac2ec1c79bc8e734.

--- keymaster_qcom.cpp
+++ keymaster_qcom.cpp
@@ -27,6 +27,7 @@
 #include <openssl/err.h>
 #include <openssl/x509.h>
 
+#include <utils/UniquePtr.h>
 #include <linux/ioctl.h>
 #include <linux/msm_ion.h>
 #include <sys/mman.h>
@@ -41,8 +42,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dlfcn.h>
-
-#include <UniquePtr.h>
 
 #include "QSEEComAPI.h"
 #include "keymaster_qcom.h"
